cff8a30ae280927a82070f984378649fcbe3ba23
kamil <kamil@chicos.life>
2022-03-25 20:49:49 +0100

    Remove db name

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 
 const app = express();
-const URL = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWD}@${process.env.DB_NAME}.z6bd2.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+
+const USER = process.env.DB_USER;
+const DB_NAME = process.env.DB_NAME;
+const PASSWD = process.env.PASSWD;
+const URL = `mongodb+srv://${USER}:${PASSWD}@${DB_NAME}.z6bd2.mongodb.net/Cluster0?retryWrites=true&w=majority`;
 const client = new MongoClient(URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/public'))
-const db = client.db("Homie");
+const db = client.db(DB_NAME);
 
 app.get('/',(req, res) => {
   res.json('Hello there')
